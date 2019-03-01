738683f4c23480d59d8f33d902825a0046b2464d
Kate Sanders <SandersKM@Hendrix.edu>
2019-03-01 09:52:29 -0600

    added figure folder

--- Variation/Mathlevel_example.R
+++ Variation/Mathlevel_example.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(Ecdat)
 library(flextable)
 library(ggplot2)
+library(scales)
 
 # First, we will save the data as a variable in RStudio
 data("Mathlevel")
@@ -15,7 +16,6 @@ diff(sat_range)
 # The IQR is just 
 IQR(Mathlevel$sat)
 
-
 # Let's find the variance
 var(Mathlevel$sat)
 
@@ -24,7 +24,11 @@ var(Mathlevel$sat)
 # Let's find the standard deviations
 sd(Mathlevel$sat)
 
+# Make
+means_language = Mathlevel %>% group_by(language) %>% summarize_at(vars(sat), funs(mean, sd))
 
-
+ggplot(means_language) +
+  geom_bar( aes(x=language, y=mean), stat="identity", fill="skyblue", alpha=0.7) + ylab("Mean SAT core") +
+  geom_errorbar( aes(x=language, ymin=mean-sd, ymax=mean+sd), width=0.4, colour="orange", alpha=0.9, size=1.3)
 
 
